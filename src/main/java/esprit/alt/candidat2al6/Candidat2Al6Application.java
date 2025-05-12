9a017b6786912b632c229938eb4d2c89c98d9681
oumeima-ibnelfekih <oumeima.ibnelfekih@esprit.tn>
2025-05-12 11:07:54 +0100

    add eureka config

--- src/main/java/esprit/alt/candidat2al6/Candidat2Al6Application.java
+++ src/main/java/esprit/alt/candidat2al6/Candidat2Al6Application.java
@@ -3,9 +3,12 @@ package esprit.alt.candidat2al6;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Candidat2Al6Application {
 
